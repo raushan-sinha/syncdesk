38cc1c2c2da9dabf0382bca7c23d66d9a451ded3
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-05-29 22:15:19 +0530

    Added code

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,13 @@
-import { Text, View } from "react-native";
+import { Navbar } from "@/components/navigation/TopNavigation";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    return (
+        <SafeAreaView>
+            <View>
+                <Navbar />
+            </View>
+        </SafeAreaView>
+    );
 }
