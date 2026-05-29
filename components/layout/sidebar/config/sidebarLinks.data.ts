0eea2eed0a57a13e6cf704ac72bee772e79e338d
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-05-29 23:04:54 +0530

    Added Menu data

---
+++ components/layout/sidebar/config/sidebarLinks.data.ts
@@ -0,0 +1,28 @@
+interface MenuProps {
+    id: number,
+    title: string,
+    icon: string
+}
+
+export const menuGroups: MenuProps[][] = [
+    [
+        { id: 1, title: 'Dashboard', icon: 'dashboard' },
+        { id: 2, title: 'Tickets', icon: 'local-activity' },
+        { id: 3, title: 'Live Support Chat', icon: 'support-agent' },
+        { id: 4, title: 'Notifications', icon: 'notifications' },
+    ],
+    [
+        { id: 5, title: 'Devices', icon: 'devices' },
+        { id: 6, title: 'Knowledge Base', icon: 'menu-book' },
+        { id: 7, title: 'Analytics', icon: 'analytics' },
+        { id: 8, title: 'AI Assistant', icon: 'smart-toy' },
+    ],
+    [
+        { id: 9, title: 'Settings', icon: 'settings' },
+        { id: 0, title: 'Help & Support', icon: 'help-outline' },
+        { id: 1, title: 'About App', icon: 'info-outline' },
+    ],
+    [
+        { id: 12, title: 'Logout', icon: 'logout' },
+    ]
+];
