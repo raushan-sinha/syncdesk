e79e21f944fed70942bc58e04c01e02a91923875
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-05-29 23:06:12 +0530

    Added logic for close & open Sidebar

--- components/navigation/Navbar.tsx
+++ components/navigation/Navbar.tsx
@@ -1,15 +1,19 @@
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import EvilIcons from '@expo/vector-icons/EvilIcons';
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 import Ionicons from '@expo/vector-icons/Ionicons';
-import EvilIcons from '@expo/vector-icons/EvilIcons';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Sidebar from '../layout/sidebar/Sidebar';
 
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+
     return (
         <>
             <View style={styles.navbar}>
                 <View style={styles.leftSection}>
-                    <MaterialIcons name="menu-open" size={32} color="black" />
+                    <MaterialIcons name="menu-open" size={32} color="black" onPress={() => setIsOpen(!isOpen)} />
                     <Text style={styles.logoText}>SyncDesk</Text>
                 </View>
 
@@ -19,6 +23,10 @@ export default function Navbar() {
                     <EvilIcons name="user" size={34} color="black" />
                 </View>
             </View>
+
+            {isOpen && (
+                <Sidebar onClose={() => setIsOpen(false)} />
+            )}
         </>
     )
 }
